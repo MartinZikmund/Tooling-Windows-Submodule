c052b3785b26194351687e16cf7af03b02d75849
Martin Zikmund <martin@zikmund.dev>
2026-02-24 14:55:20 +0100

    Add Uno.Sdk project head

---
+++ ProjectHeads/AllComponents/Uno/Platforms/Android/Main.Android.cs
@@ -0,0 +1,19 @@
+using Android.App;
+using Android.Runtime;
+
+namespace CommunityToolkit.App.Uno;
+
+[global::Android.App.ApplicationAttribute(
+    Label = "@string/ApplicationName",
+    Icon = "@mipmap/iconapp",
+    LargeHeap = true,
+    HardwareAccelerated = true,
+    Theme = "@style/AppTheme"
+)]
+public class Application : Microsoft.UI.Xaml.NativeApplication
+{
+    public Application(IntPtr javaReference, JniHandleOwnership transfer)
+        : base(() => new CommunityToolkit.App.Shared.App(), javaReference, transfer)
+    {
+    }
+}
---
+++ ProjectHeads/SingleComponent/Uno/Platforms/iOS/Main.iOS.cs
@@ -0,0 +1,17 @@
+using Uno.UI.Hosting;
+using CommunityToolkit.App.Shared;
+
+namespace CommunityToolkit.App.Uno;
+
+public class EntryPoint
+{
+    public static void Main(string[] args)
+    {
+        var host = UnoPlatformHostBuilder.Create()
+            .App(() => new App())
+            .UseAppleUIKit()
+            .Build();
+
+        host.Run();
+    }
+}
